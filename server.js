04a42385004347a84ed95359d3424f280179a344
Ruchi Mandliya <rmandliya18@gmail.com>
2023-04-27 22:19:24 +0530

    backend setup,listen port and mongodb connection

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const connectDb = require("./config/db.js");
+// config dot env file
+dotenv.config();
+
+//databse call
+connectDb();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+//port
+const PORT = 8000 || process.env.PORT;
+
+//listen server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
